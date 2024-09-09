2460c0c46a2c5461671933b777127b555354f2f5
Melvunx <augustine.melv@gmmail.com>
2024-09-09 15:41:04 +0200

    pre-final commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const countriesContainer = document.querySelector(".countries-container");
-const search = document.getElementById("inputSearch");
-const inputRange = document.getElementById("inputRange");
-const rangeValue = document.getElementById("rangeValue");
+
 let countryData = [];
 
 // Fonction pour récupérer les données des pays
@@ -27,7 +25,9 @@ const countryDisplay = async () => {
                 <img src=${country.flags.png} alt=${country.flags.alt}>
               </div>
               <div class="card-content">
-                <p>Population : ${country.population}</p>
+                <p>Population : ${country.population
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, " ")}</p>
                 <h2>${country.name.common}</h2>
               </div>
             </div>
@@ -39,7 +39,7 @@ const countryDisplay = async () => {
 
   // Filtrer les pays en fonction de l'input de recherche
   const filterCountries = () => {
-    const searchValue = search.value.toLowerCase();
+    const searchValue = inputSearch.value.toLowerCase();
     const filteredCountries = countryData.filter((country) =>
       country.name.common.toLowerCase().includes(searchValue)
     );
@@ -54,8 +54,43 @@ const countryDisplay = async () => {
   });
 
   // Gérer la recherche des pays en fonction de la saisie utilisateur
-  search.addEventListener("input", () => {
+  inputSearch.addEventListener("input", () => {
     filterCountries();
+  });
+
+  const acsSort = () => {
+    return countryData.sort((a, b) => a.population - b.population);
+  };
+
+  const descSort = () => {
+    return countryData.sort((a, b) => b.population - a.population);
+  };
+
+  const alphaSort = () => {
+    return countryData.sort((a, b) => {
+      if (a.name.common < b.name.common) {
+        return -1;
+      } else if (a.name.common > b.name.common) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  minToMax.addEventListener("click", () => {
+    displayCountries(acsSort());
+    console.log("sort asc");
+  });
+
+  maxToMin.addEventListener("click", () => {
+    displayCountries(descSort());
+    console.log("sort desc");
+  });
+
+  alpha.addEventListener("click", () => {
+    displayCountries(alphaSort());
+    console.log("sorted by alphabet");
   });
 
   // Affichage initial avec tous les pays
